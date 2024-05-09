2a6b2952a139b8ea844529aeb413a16e155ebfcf
Charles Enright <c.enright@me.com>
2024-05-09 14:40:00 +0000

    Add Bing sat imagery

--- assets/script.js
+++ assets/script.js
@@ -21,22 +21,10 @@ layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
 });
 
  // Marker at Swansea
- var marker = L.marker([51.62144, -3.943645]).addTo(map);
- marker.bindPopup("<b>Hello Swansea!</b>").openPopup();
+ var markerSwansea = L.marker([51.62144, -3.943645]).addTo(map);
+ markerSwansea.bindPopup("<b>Hello Swansea!</b>").openPopup();
 
-  // Example sites
-  var marker1 = L.marker([52.075744 , -4.7382202]).addTo(map);
-  marker.bindPopup("<b>Example</b>").openPopup();
-
-  var marker2 = L.marker([51.819996, -4.76665]).addTo(map);
-  marker.bindPopup("<b>Example</b>").openPopup();
-
-  var marker3 = L.marker([51.831249, -4.800618]).addTo(map);
-  marker.bindPopup("<b>Example</b>").openPopup();
-
-
-  var marker4 = L.marker([51.85926, -4.849777]).addTo(map);
-  marker.bindPopup("<b>Example</b>").openPopup();
+ 
 
  // Function to show location map clicked
  function onMapClick(e) {
@@ -59,4 +47,15 @@ map.on('click', onMapClick);
     L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
+ // Example sites
+ var marker1 = L.marker([52.075744 , -4.7382202]).addTo(map);
+ marker1.bindPopup("<b>Example</b>").openPopup();
 
+ var marker2 = L.marker([51.819996, -4.76665]).addTo(map);
+ marker2.bindPopup("<b>Example</b>").openPopup();
+
+ var marker3 = L.marker([51.831249, -4.800618]).addTo(map);
+ marker3.bindPopup("<b>Example</b>").openPopup();
+
+ var marker4 = L.marker([51.85926, -4.849777]).addTo(map);
+ marker4.bindPopup("<b>Example</b>").openPopup();
