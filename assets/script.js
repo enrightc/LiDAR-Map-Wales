9dcaa46269f546ee1408583cfe01c3159a1f8129
Charles Enright <c.enright@me.com>
2024-05-09 14:03:04 +0000

    Add LiDAR examples to map

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,11 @@ var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Bing Maps Satellite Layer
+var bingSatellite = new L.BingLayer('AijylEokpVtrqizDDsnkC0MjYi0bCpwSDHLHCeExE8aN965nj6fIcQFv33C1wisv', {
+    type: 'Aerial'  // Use 'AerialWithLabels' if you want satellite images with labels
+});
+
 
 // Adding the WMS layer for LiDAR DSM (hillshade) data
 var wmsLayer = L.tileLayer.wms("https://datamap.gov.wales/geoserver/ows", { // Base URL to the OWS endpoint
@@ -18,6 +23,20 @@ layers: 'geonode:wales_lidar_dsm_1m_hillshade_cog',
  // Marker at Swansea
  var marker = L.marker([51.62144, -3.943645]).addTo(map);
  marker.bindPopup("<b>Hello Swansea!</b>").openPopup();
+
+  // Example sites
+  var marker = L.marker([52.075744 , -4.7382202]).addTo(map);
+  marker.bindPopup("<b>Example</b>").openPopup();
+
+  var marker = L.marker([51.819996, -4.76665]).addTo(map);
+  marker.bindPopup("<b>Example</b>").openPopup();
+
+  var marker = L.marker([51.831249, -4.800618]).addTo(map);
+  marker.bindPopup("<b>Example</b>").openPopup();
+
+
+  var marker = L.marker([51.85926, -4.849777]).addTo(map);
+  marker.bindPopup("<b>Example</b>").openPopup();
 
  // Function to show location map clicked
  function onMapClick(e) {
@@ -29,7 +48,8 @@ map.on('click', onMapClick);
 
     // Layer control to toggle between layers
     var baseMaps = {
-        "OpenStreetMap": openStreetMap
+        "OpenStreetMap": openStreetMap,
+        "Bing Satellite": bingSatellite
     };
 
     var overlayMaps = {
